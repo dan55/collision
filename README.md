1be09ec7a221738924b5d2f8833b5710750d411e
dan55 <dan.elkin584@gmail.com>
2019-07-20 10:50:50 -0400

    add readme and test_request

---
+++ README.md
@@ -0,0 +1,5 @@
+This serves a tensorflow model for detecting collisions.
+
+The API expects a string representing an image at the /predict endpoint.
+
+See the `test_request.py` script for an example.
--- test_request.py
+++ test_request.py
@@ -3,7 +3,8 @@ import json
 
 # https://gist.github.com/kylehounslow/767fb72fde2ebdd010a0bf4242371594
 
-URL = 'http://localhost:5000/predict'
+# URL = 'http://localhost:5000/predict'
+URL = 'https://vehicle-collision.herokuapp.com/predict'
 
 content_type = 'image/jpeg'
 headers = {'content-type': content_type}
